2e3dbaaf02c3b39c1a82ffe7bd7dbbdbb92ec1e3
Teuassuri Gomes <teuassuri@gmail.com>
2022-06-18 23:13:03 -0300

    Mudança na api
    Mudança na api

--- routes/api.php
+++ routes/api.php
@@ -22,32 +22,5 @@ use Illuminate\Support\Facades\Route;
  * 
  */
 
-Route::group(['prefix' => 'rest'], function () {
-    Route::get('weather/forecast', 'ScheduleStudentController@weatherForecast');
-    Route::get('level/index', 'LevelController@indexApi');
-    
-    
-    Route::group(['prefix' => 'user'], function () {
-        Route::get('index', 'UserController@indexApi');
-        Route::post('store', 'UserController@storeApi');
-        Route::post('edit/{id}', 'UserController@editApi');
-        Route::get('remover/{id}', 'UserController@removerApi');
-    });
 
-    Route::group(['prefix' => 'group'], function () {
-        Route::get('index', 'GroupController@indexApi');
-        Route::post('store', 'GroupController@storeApi');
-        Route::post('edit/{id}', 'GroupController@editApi');
-        Route::post('remover/{id}', 'GroupController@removerApi');
-    });
-
-    Route::group(['prefix' => 'student'], function () {
-        Route::get('index', 'StudentController@indexApi');
-        Route::get('group/{group_id}', 'StudentController@studentByGroup');
-        Route::post('store', 'StudentController@storeApi');
-        Route::post('edit/{id}', 'StudentController@editApi'); 
-        Route::get('delete/{id}', 'StudentController@deleteApi');
-    });
-
-});
 
